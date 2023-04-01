8da64b74580482899b89fab5f20c8385a876b1db
Highbrook <MilivojevicDjole@yahoo.com>
2023-04-01 13:04:13 +0200

    search
    added search function

---
+++ XmlFinder/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace XmlFinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("asdadss");
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.resultListView = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.keywordTextBox = new System.Windows.Forms.TextBox();
+            this.folderPathTextBox = new System.Windows.Forms.TextBox();
+            this.folderPathButton = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.FileName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1018, 497);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.searchButton);
+            this.tabPage1.Controls.Add(this.folderPathButton);
+            this.tabPage1.Controls.Add(this.folderPathTextBox);
+            this.tabPage1.Controls.Add(this.keywordTextBox);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.resultListView);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1010, 471);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Search in Files";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1010, 471);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Replace in Files";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // resultListView
+            // 
+            this.resultListView.Alignment = System.Windows.Forms.ListViewAlignment.Left;
+            this.resultListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.FileName});
+            this.resultListView.HideSelection = false;
+            this.resultListView.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1});
+            this.resultListView.Location = new System.Drawing.Point(13, 106);
+            this.resultListView.Name = "resultListView";
+            this.resultListView.Size = new System.Drawing.Size(991, 359);
+            this.resultListView.TabIndex = 0;
+            this.resultListView.UseCompatibleStateImageBehavior = false;
+            this.resultListView.View = System.Windows.Forms.View.Details;
+            this.resultListView.SelectedIndexChanged += new System.EventHandler(this.resultListView_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Result of keyword search:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Keyword";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(456, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Folder path";
+            // 
+            // keywordTextBox
+            // 
+            this.keywordTextBox.Location = new System.Drawing.Point(13, 29);
+            this.keywordTextBox.Name = "keywordTextBox";
+            this.keywordTextBox.Size = new System.Drawing.Size(440, 20);
+            this.keywordTextBox.TabIndex = 4;
+            // 
+            // folderPathTextBox
+            // 
+            this.folderPathTextBox.Location = new System.Drawing.Point(459, 29);
+            this.folderPathTextBox.Name = "folderPathTextBox";
+            this.folderPathTextBox.Size = new System.Drawing.Size(432, 20);
+            this.folderPathTextBox.TabIndex = 5;
+            this.folderPathTextBox.TextChanged += new System.EventHandler(this.folderPathTextBox_TextChanged);
+            // 
+            // folderPathButton
+            // 
+            this.folderPathButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.folderPathButton.Location = new System.Drawing.Point(898, 29);
+            this.folderPathButton.Name = "folderPathButton";
+            this.folderPathButton.Size = new System.Drawing.Size(106, 20);
+            this.folderPathButton.TabIndex = 6;
+            this.folderPathButton.Text = "Open";
+            this.folderPathButton.UseVisualStyleBackColor = true;
+            this.folderPathButton.Click += new System.EventHandler(this.folderPathButton_Click);
+            // 
+            // searchButton
+            // 
+            this.searchButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.searchButton.Location = new System.Drawing.Point(13, 55);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(106, 20);
+            this.searchButton.TabIndex = 7;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // FileName
+            // 
+            this.FileName.Text = "File";
+            this.FileName.Width = 990;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1042, 521);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Xml Finder";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button folderPathButton;
+        private System.Windows.Forms.TextBox folderPathTextBox;
+        private System.Windows.Forms.TextBox keywordTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView resultListView;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.ColumnHeader FileName;
+    }
+}
+
---
+++ XmlFinder/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace XmlFinder
+{
+    public partial class Form1 : Form
+    {
+        private FolderBrowserDialog folderBrowserDialog;
+        private List<string> allItems = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void folderPathButton_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog = new FolderBrowserDialog();
+            DialogResult result = folderBrowserDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                folderPathTextBox.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+        private void folderPathTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo dir = new DirectoryInfo(folderPathTextBox.Text);
+            FileInfo[] dirFilesCount = dir.GetFiles("*");
+
+            foreach (var fileName in dirFilesCount)
+            {
+                resultListView.Items.Add(fileName.ToString());
+            }
+
+            foreach (var item in resultListView.Items)
+            {
+                this.allItems.Add(item.ToString());
+            }
+        }
+        // TODO Add windows explorer opening when you select a file and navigate to its location
+        private void resultListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (resultListView.SelectedItems.Count == 0)
+                return;
+            else
+            {
+                string selected = resultListView.SelectedItems[0].Text;
+                OpenFileDialog dialog = new OpenFileDialog();
+                DialogResult dialogResult = MessageBox.Show(selected);
+            }
+        }
+
+        private void OpenFolder(string folderPath)
+        {
+            if (Directory.Exists(folderPath))
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo(folderPath, "explorer.exe");
+                
+                Process.Start(startInfo);
+            }
+            else
+            {
+                MessageBox.Show(string.Format("{0} Directory does not exist!", folderPath));
+            }
+        }
+    }
+}
+
+
+//{
+//    Arguments = folderPath,
+//    FileName = "explorer.exe";
+//};
